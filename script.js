0087a12dd4138db75dc7c32cb496289f0ca1f307
romek-krasnoperov <115365225+romek-krasnoperov@users.noreply.github.com>
2023-04-09 22:20:11 +0200

    pBtn.disabled to prevent sending the fom with un match passwords

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
 const passWord = document.querySelector("[data-psw]")
 const passConfirm = document.querySelector("[data-psw-con]")
 const pMsg = document.querySelector("[data-p-msg]")
+const pBtn = document.querySelector("[data-btn]")
 
 let ConfirmPassword = function () {
     if (passWord.value === passConfirm.value){
         pMsg.className = "green"
         pMsg.innerHTML = "Passwords match!"
         pMsg.style.visibility = "visible"
+        pBtn.disabled = false
     } else {
         pMsg.className = "red"
         pMsg.innerHTML = "Passwords do NOT match!"
         pMsg.style.visibility = "visible"
+        pBtn.disabled = true
     }
 }
